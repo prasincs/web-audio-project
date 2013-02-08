0f946553224ecb90340bb03cc9283daadf791011
gautam, prasanna <prasannagautam@gmail.com>
2013-02-08 16:30:54 -0500

    heroku support

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring "1.1.6"]]
   :plugins [[lein-cljsbuild "0.3.0"]]
-  :profiles {:dev {
   :hooks [leiningen.cljsbuild]
   :cljsbuild { 
     :builds {
@@ -21,7 +20,7 @@
       ;https://github.com/emezeske/lein-cljsbuild/blob/master/example-projects/advanced/project.clj
         ;:repl-listen-port 9000
       }
-    }}}}
+    }}
   }
   :main web-audio-project.server)
 
